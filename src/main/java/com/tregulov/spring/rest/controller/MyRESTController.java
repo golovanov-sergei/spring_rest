60aa4c208b98a8f180877eef999bbccc4beafbf3
imp <imp.sergei@gmail.com>
2021-07-20 18:32:26 +0300

    87. DELETE

--- src/main/java/com/tregulov/spring/rest/controller/MyRESTController.java
+++ src/main/java/com/tregulov/spring/rest/controller/MyRESTController.java
@@ -18,17 +18,38 @@ public class MyRESTController {
     private EmployeeService employeeService;
 
     @GetMapping("/employees")
-    public List<Employee> getAllEmployees(){
+    public List<Employee> getAllEmployees() {
         List<Employee> allEmployees = employeeService.getAllEmployees();
-        return  allEmployees;
+        return allEmployees;
     }
+
     @GetMapping("/employees/{id}")
-    public Employee getEmployee(@PathVariable int id){
+    public Employee getEmployee(@PathVariable int id) {
         Employee employee = employeeService.getEmployee(id);
-        if (employee==null){
-            throw new NoSuchEmployeeException("Employee with ID ("+id+") not found");
+        if (employee == null) {
+            throw new NoSuchEmployeeException("Employee with ID (" + id + ") not found");
         }
         return employee;
     }
 
+    @PostMapping("/employees")
+    public Employee addNewEmployee(@RequestBody Employee employee) {
+        employeeService.saveEmployee(employee);
+        return employee;
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee) {
+        employeeService.saveEmployee(employee);
+        return employee;
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public String deleteEmployee(@PathVariable int id) {
+        Employee employee = employeeService.getEmployee(id);
+        if (employee == null) throw new NoSuchEmployeeException("Employee with ID (" + id + ") not found");
+
+        employeeService.deleteEmployee(id);
+        return "Employee with ID = " + id + " was deleted";
+    }
 }
